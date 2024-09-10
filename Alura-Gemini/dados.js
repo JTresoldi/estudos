f8866a2f3c88f0a2563ac0a833d77cf0b0516fec
JTresoldi <jaaao.a7x@gmail.com>
2024-09-09 22:25:43 -0300

    att

---
+++ Alura-Gemini/dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a 'Fadinha', é uma das maiores skatistas do mundo. Com apenas [idade da Rayssa], ela já conquistou diversos títulos e se tornou um ícone do esporte, inspirando jovens skatistas em todo o mundo. Sua habilidade e carisma a tornaram uma das atletas mais populares do Brasil.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é, sem dúvida, um dos maiores nomes do esporte brasileiro da atualidade. A ginasta, com sua força, agilidade e determinação, conquistou o coração dos brasileiros e se tornou um ícone da modalidade. Com diversas medalhas em competições nacionais e internacionais, incluindo o ouro olímpico, Rebeca representa a nova geração de atletas brasileiros, que busca sempre superar seus limites e levar o nome do país para o topo do pódio. Sua trajetória inspiradora e seu talento inegável a colocam como uma das maiores ginastas da história do Brasil",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela representa o futuro do judô brasileiro. Sua trajetória inspiradora e seu talento inegável a colocam como uma das maiores promessas do esporte.",
+        link : "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    }
+]
